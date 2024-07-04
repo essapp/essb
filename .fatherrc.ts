d3ec56b7349afe5a29abe8cda1113242b89263ed
zhaoluchang <zhaolc@ess.pub>
2024-07-04 12:19:19 +0800

    mod git

--- .fatherrc.ts
+++ .fatherrc.ts
@@ -14,7 +14,7 @@ export default defineConfig({
     extraBabelPlugins: [[require.resolve('./scripts/replaceEs'), {}]],
     input: 'src', // 默认编译目录
     output: 'lib',
-    platform: 'browser', // 默认构建为 Node.js 环境的产物
-    transformer: 'babel', // 默认使用 esbuild 以获得更快的构建速度
+    platform: 'node', // 默认构建为 Node.js 环境的产物
+    transformer: 'esbuild', // 默认使用 esbuild 以获得更快的构建速度
   },
 });
